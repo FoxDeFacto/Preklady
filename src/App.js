f585e1ca295a19a0543d6a3eed601bf7fcd02be4
FoxDeFacto <lisakzezhore@seznam.cz>
2024-09-20 23:05:32 +0200

    Hashrouter fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -8,19 +8,21 @@ import TranslationDetail from './pages/TranslationDetail';
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/all" element={<AllTranslations />} />
-            <Route path="/detail/:id" element={<TranslationDetail />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <HashRouter>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/all" element={<AllTranslations />} />
+              <Route path="/detail/:id" element={<TranslationDetail />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </HashRouter>
   );
 };
 
